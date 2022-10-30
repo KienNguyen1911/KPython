08ae46ee77d4695dc510b02fc9f09d29c49121c4
KienNguyen1911 <ngkien1911@gmail.com>
2022-10-30 19:49:11 +0700

    function | indexing | returnStatement

--- Basic Python/indexing.py
+++ Basic Python/indexing.py
@@ -1,1 +1,14 @@
 # index operator[] = gives access to sequence's element (str, list , tuples)
+
+name = "kien Nguyen!"
+
+if name[0].islower():
+    name = name.capitalize()
+
+first_name = name[:4].upper()
+last_name = name[:5].lower()
+last_chacter = name[:]
+
+print(first_name)
+print(last_name)
+print(last_chacter)
---
+++ function.py
@@ -0,0 +1,10 @@
+# function
+
+def hello(first_name, last_name, age):
+    print("Hello "+first_name+" "+last_name)
+    print("You are " +str(age) + " years old")
+    print("Have a nice day!!")
+
+fname = "Nguyen"
+lname = "Kien"
+hello(fname, lname, 20)
